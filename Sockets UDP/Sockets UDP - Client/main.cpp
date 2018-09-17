3e882e9c379864e2212dbba6a572ba06596ecaf6
cumus <ec4who@hotmail.com>
2018-09-17 11:19:42 +0200

    Added Project with Client & Server
    solution-properties command arguments:
    added 127.0.0.1 8000 to client
    added 8000 to server

---
+++ Sockets UDP/Sockets UDP - Client/main.cpp
@@ -0,0 +1,13 @@
+
+// Client
+
+// Defined inside project properties - input dependancies
+// #pragma comment(lib, "ws2_32.lib")
+
+int main(int argc, char **argv)
+{
+	char* p = argv[argc];
+	return 0;
+}
+
+
---
+++ Sockets UDP/Sockets UPD - Server/main.cpp
@@ -0,0 +1,12 @@
+
+// Server
+
+// Defined inside project properties - input dependancies
+// #pragma comment(lib, "ws2_32.lib")
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
+
+
